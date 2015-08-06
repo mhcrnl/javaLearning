22d18f72be38013b4db6f88164918e327370a192
Nikita <nsalomatin@hotmail.com>
2015-08-07 01:04:09 +0400

    Added: Find max and min elements, more details you can see inside

--- src/com/epam/nikita_salomatin/Array.java
+++ src/com/epam/nikita_salomatin/Array.java
@@ -7,7 +7,7 @@ public class Array {
     
     private int[] myArray;
 	private static int arrayLength, arrayMinValue, arrayMaxValue, taskNumber;
-	
+	//Получение нужной информации
 	private void toPrepare() {
 		
 		 Scanner in = new Scanner(System.in); 
@@ -38,7 +38,7 @@ public class Array {
 		
 		Array array = new Array();
 		myArray = new int [arrayLength];
-		
+		//Создание исходного массива
 		System.out.print("Array:=");
 	    for(int i=0;i<myArray.length;i++) {
 	    myArray[i] = array.getRandomValue(arrayMinValue, arrayMaxValue);
@@ -48,22 +48,60 @@ public class Array {
 	     
 	}
 	
-	
+	//Решения задач
 	public void modify(int taskNumber) {
 		
 		
 		
 		switch (taskNumber) {
 	
-        case 1:   
-        		 
+        case 1:  //В массиве целых чисел поменять местами максимальный отрицательный элемент и минимальный положительный
+        
         		 try {
         			 
-        			 System.out.print("\n" + "Modified array:=");
+        			 System.out.println("\n" + "Modified array:=");
+        			 //Получаем исходный массив
         			 int modifiedArray[] = getArray();
+        			 int maxElem = arrayMaxValue, minElem = 0, countNegativeValue = 0, countPositiveValue = 0, positionMin = 0, positionMax = 0;
         			 for(int i=0;i<modifiedArray.length;i++) {
-            			 
-            			 System.out.print(modifiedArray[i] + " ");
+            			
+            			//Есть ли отриц. элементы
+            			if (modifiedArray[i]<0) {
+            				countNegativeValue++;
+            			}
+            			//Если нету, выходим
+            			if (i==arrayLength-1 && countNegativeValue==0) {
+            				System.out.println("There are no negative values");
+            				break;
+            			}
+            			
+            			//Есть ли положит. элементы
+            			if (modifiedArray[i]>=0) {
+            				countPositiveValue++;
+            				//Поиск минимального положительного
+            				if (modifiedArray[i]<maxElem) {
+            					maxElem=modifiedArray[i];
+            					positionMax = i+1;
+            					
+            				}
+            			}
+            			//Если нету, выходим
+            			if (i==arrayLength-1 && countPositiveValue==0) {
+            				System.out.println("There are no positive values");
+            				break;
+            			}
+            			
+            			//Поиск максимально отрицательного
+            			if (modifiedArray[i]<minElem) {
+            				minElem=modifiedArray[i];
+            				positionMin = i+1;
+            			}
+            			//Вывод значения и позиции
+            			if (i==arrayLength-1) {
+        					System.out.println("maxElem=" + maxElem + " " + "Position=" + positionMax);
+        					System.out.println("minElem=" + minElem + " " + "Position=" + positionMin);
+        				}
+            		
             		 }
             		 
         		  }  catch (NullPointerException e) {
@@ -73,15 +111,11 @@ public class Array {
         		 
         		 break;
         		
-        		 
-        		 
-        		 
-        
-        default: System.out.println("\n" + "Invalid task number");
+       default: System.out.println("\n" + "Invalid task number");
                  break;
         }
 	}
-	
+	   //Рандом из кастомного диапозона
 	private int getRandomValue(int min, int max) {
 	   int range = (max - min) + 1;     
 	   return (int)(Math.random() * range) + min;
